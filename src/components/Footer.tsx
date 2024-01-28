4dddc8ac872c85af61cc55ad80cc58f27c8d0239
diamuji <diamuji1@gmail.com>
2024-01-28 02:18:09 -0800

    dashboard

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
+      <div className="flex flex-grow items-center justify-between  py-2 px-4 shadow-2 md:px-6 2xl:px-11">
+        <div className="flex  gap-10 w-full p-4 justify-center">
+          <span>Streamer name</span>
+          <span>-Connected-</span>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
