38f02efcfce18584d3a1b5eeb52fd4902097e4e5
diamuji <diamuji1@gmail.com>
2024-01-29 09:00:22 -0800

    fix text size for resoultuib

--- src/components/LeftPanel.tsx
+++ src/components/LeftPanel.tsx
@@ -5,19 +5,27 @@ const LeftPanel = () => {
     <div className="left-panel w-full h-full flex flex-col justify-start items-center bg-[#98a8e2] rounded-lg">
       <div className="w-full h-fit px-4 py-4 flex hover:cursor-pointer border border-[#98a8e2] hover:border-white text-white gap-4 rounded-t-lg">
         <DimIcon />
-        <span className="text-4xl font-semibold">Dim</span>
+        <span className="2xl:text-4xl md:text-xl text-lg font-semibold">
+          Dim
+        </span>
       </div>
       <div className="w-full h-fit px-4 py-4 flex hover:cursor-pointer border border-[#98a8e2] hover:border-white gap-4 text-white">
         <RecordIcon />
-        <span className="text-4xl font-semibold">Cap-True</span>
+        <span className="2xl:text-4xl md:text-xl text-lg font-semibold">
+          Capture
+        </span>
       </div>
       <div className="w-full h-fit px-4 py-4 flex hover:cursor-pointer border border-[#98a8e2] hover:border-white gap-4 text-white">
         <CameraIcon />
-        <span className="text-4xl font-semibold">Play Back</span>
+        <span className="2xl:text-4xl md:text-xl text-lg font-semibold">
+          Play Back
+        </span>
       </div>
       <div className="w-full h-fit px-4 py-4 flex hover:cursor-pointer border border-[#98a8e2] hover:border-white gap-4 text-white rounded-b-lg">
         <ControlIcon />
-        <span className="text-4xl font-semibold">Param</span>
+        <span className="2xl:text-4xl md:text-xl text-lg font-semibold">
+          Param
+        </span>
       </div>
     </div>
   );
