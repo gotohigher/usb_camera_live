3ae29b0e6da6c02307c3c4d73fc96994079c2163
diamuji <diamuji1@gmail.com>
2024-01-29 07:17:31 -0800

    feat: left and right panel

---
+++ src/components/RightPanel.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const RightPanel = () => {
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  return (
+    <div className="left-panel w-full h-full flex flex-col justify-start items-center bg-[#98a8e2] rounded-lg">
+      <div className="w-full h-fit px-4 py-4 flex gap-4 bg-[#FF9505] rounded-t-lg">
+        <span className="text-[18px] font-semibold text-white whitespace-nowrap text-center mx-auto">
+          CID: 361251608235792
+        </span>
+      </div>
+      <div className="w-full h-fit px-4 py-4 flex p-4  justify-center">
+        <button className="bg-[#FF9505] rounded-lg px-4 py-3 text-white font-semibold hover:bg-[#fdb95b]">
+          Live Stream Code
+        </button>
+      </div>
+      <div className="w-full h-fit px-4 py-4 flex hover:cursor-pointer border gap-4 border-[#98a8e2] flex-col">
+        <span className="text-4xl font-semibold text-white">Username</span>
+        <input
+          type="text"
+          className="w-full px-2 py-1 bg-black text-white text-13px font-normal text-end outline-none rounded-[6px]"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+      </div>
+      <div className="w-full h-fit px-4 py-4 flex hover:cursor-pointer border gap-4 rounded-b-lg border-[#98a8e2] flex-col">
+        <span className="text-4xl font-semibold text-white">Password</span>
+        <input
+          type="password"
+          className="w-full px-2 py-1 bg-black text-white text-13px font-normal text-end outline-none rounded-[6px]"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </div>
+    </div>
+  );
+};
+export default RightPanel;
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,12 +1,24 @@
+import LeftPanel from "../../components/LeftPanel";
+import RightPanel from "../../components/RightPanel";
+
 const Dashboard = () => {
   return (
     <>
       <div className="grid grid-cols-10 gap-3.5 w-full h-full">
-        <div className="col-span-2 h-full bg-primary w-full"></div>
-        <div className="col-span-6 h-full bg-primary w-full">
-          <video></video>
+        <div className="col-span-2 h-full w-full">
+          <LeftPanel />
         </div>
-        <div className="col-span-2 h-full bg-primary w-full"></div>
+        <div className="col-span-6 h-full w-full rounded-lg bg-white">
+          <video className="w-full h-full" src="https://www.bigbuckbunny.org/">
+            <source
+              src="https://www.bigbuckbunny.org/"
+              type="video/mp4"
+            ></source>
+          </video>
+        </div>
+        <div className="col-span-2 h-full w-full ">
+          <RightPanel />
+        </div>
       </div>
     </>
   );
